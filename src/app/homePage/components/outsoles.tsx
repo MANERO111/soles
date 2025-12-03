2b9d4d0070a52306f0b151c000b3faf1520b1256
Amjad <akioudamjadfone2@gmail.com>
2025-12-04 00:53:23 +0100

    add fabricated navlink

--- src/app/homePage/components/outsoles.tsx
+++ src/app/homePage/components/outsoles.tsx
@@ -323,7 +323,7 @@ function PrefabricatedOutsolesSection() {
                 playsInline
                 className="w-full h-full object-cover"
               >
-                <source src="/video/Casa-Seumelle-compressed.mp4" type="video/mp4" />
+                <source src="/video/Casa Seumelle_2_compressed.mp4" type="video/mp4" />
               </video>
               
               {/* Overlay */}
